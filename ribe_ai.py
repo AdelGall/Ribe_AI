000c1afb1c31f39e5cbe4422527a6a2c0955af0e
Мурад Гаджимагомедов <affiliation@MacBook-Air-Murad.local>
2025-08-31 17:57:32 +0300

    fix_token

--- ribe_ai.py
+++ ribe_ai.py
@@ -1,26 +1,31 @@
 import asyncio
+import os
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher
+from aiogram.client.bot import DefaultBotProperties
 from aiogram.types import Message
-from aiogram.enums import ParseMode
-from aiogram import F
 from aiogram.filters import Command
 
-TOKEN = "7579930042:AAG3SBzgC-RTcGvDzf3uPCri-VU5eitbPUU"
-
-load_dotenv() 
+# Загружаем переменные окружения из .env
+load_dotenv()
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не установлен. Проверь .env файл или переменные окружения.")
-bot = Bot(token=TOKEN)
+
+# Создаём бота с новыми настройками по версии aiogram 3.7+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode="HTML")
+)
+
 dp = Dispatcher()
 
-# простой обработчик команды /start
+# Обработчик команды /start
 @dp.message(Command("start"))
-async def start_command(message: types.Message):
+async def start_command(message: Message):
     await message.answer("Привет! Я твой первый бот на aiogram 🚀")
 
+# Точка входа
 async def main():
     await dp.start_polling(bot)
 
