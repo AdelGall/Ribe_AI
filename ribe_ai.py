bb066c85f57cddbf31c8df6f1b9f40284e3e0dad
Мурад Гаджимагомедов <affiliation@MacBook-Air-Murad.local>
2025-09-06 22:42:36 +0300

    new_version

--- ribe_ai.py
+++ ribe_ai.py
@@ -1,33 +1,47 @@
-import asyncio
 import os
+import telebot
+import whisper
+
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher
-from aiogram.client.bot import DefaultBotProperties
-from aiogram.types import Message
-from aiogram.filters import Command
 
-# Загружаем переменные окружения из .env
+file_name = '.env'
+
 load_dotenv()
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не установлен. Проверь .env файл или переменные окружения.")
 
-# Создаём бота с новыми настройками по версии aiogram 3.7+
-bot = Bot(
-    token=BOT_TOKEN,
-    default=DefaultBotProperties(parse_mode="HTML")
-)
+model = whisper.load_model("base")
+token_bot = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(token_bot, parse_mode=None)
 
-dp = Dispatcher()
+dir = os.path.dirname(__file__)
 
-# Обработчик команды /start
-@dp.message(Command("start"))
-async def start_command(message: Message):
-    await message.answer("Привет! Я твой первый бот на aiogram 🚀")
+@bot.message_handler(content_types=['text'])
+def send_welcome(message):
+    bot.reply_to(message, "Здравствуй, отправь аудио-файл для транскрибации")
 
-# Точка входа
-async def main():
-    await dp.start_polling(bot)
+@bot.message_handler(content_types=['audio'])
+def echo_all(message):
+    file = bot.get_file(message.audio.file_id)
+    downloaded_file = bot.download_file(file.file_path)
+    audio_name = message.audio.file_name
+    with open(audio_name, 'wb') as f:
+        f.write(downloaded_file)
+    bot.reply_to(message, "Файл получен, начинается транскрибация, это может занять какое-то время...")
+    result = transcribe_audio(f'{dir}/{audio_name}')
+    send_long_message(message.chat.id, result)
+    bot.reply_to(message, "Конец транскрибации")
+    os.remove(f'{dir}/{audio_name}')
 
-if __name__ == "__main__":
-    asyncio.run(main())
+def send_long_message(chat_id, text):
+    max_len = 4096
+    for i in range(0, len(text), max_len):
+        chunk = text[i:i+max_len]
+        if len(chunk) > max_len:
+            chunk = chunk[:max_len]
+        bot.send_message(chat_id, chunk)
+
+
+def transcribe_audio(file_path):
+    result = model.transcribe(file_path)
+    return result["text"]
+
+bot.polling(none_stop=True)
